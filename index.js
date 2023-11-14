26966d8f4765d3ea8ddbbdacec80081a8596f674
Dimitrije <dimitrijezdravkovic20@gmail.com>
2023-11-14 22:04:27 +0100

    init

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const express = require("express");
 const onServerStart = require("./startDatabase");
 
+//cache the data from csv file and calculate the required statistics in this object
+//in form of {key, value} pairs, with key being the player fullName and value stats for him
 let statistics = {};
 const app = express();
 
+//before the server starts, collect the data from csv file and process it
+//store stats in the "statistics" object
+//if the data is collected correctly, callback function will be called (server will start)
 function onStartUP(callback) {
     onServerStart()
         .then(returnObject => {
             statistics = returnObject;
+            console.log("In-memory database is UP!");
+            callback();
         })
         .catch(error => {
             console.log('Error', error);
         });
-    callback();
 }
 
 onStartUP(() => app.listen(3000, () => console.log("Server listening at port 3000")));
 
+//define endpoints
+//valid playerName results in JSON response (stats)
 app.get("/stats/player/:playerFullName", (req, res) => {
-    if (!(req.params.playerFullName in statistics)) {
+    if (!(req.params.playerFullName in statistics))
         res.status(404).send();
-    }
+
     else {
         res.status(200);
         res.setHeader('Content-Type', 'application/json');
--- startDatabase.js
+++ startDatabase.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const { parse} = require('csv-parse');
 
+//round a number to one decimal if it has more than one,
+//if dividing by 0, return 0 instead of null
 function roundOneDecimal(number) {
-    return Math.round(number * 10) / 10;
+    return (number) ? Math.round(number * 10) / 10 : 0;
 }
 
-//collect the data from all matches
+//if player hasn't got any stats just store new stats,
+//else update old stats (sum up every parameter of both)
 function update(array, data, playerFullName) {
     if (!(playerFullName in array))
         array[playerFullName] = data;
@@ -16,13 +19,14 @@ function update(array, data, playerFullName) {
     return array;
 }
 
-//calculate statistics
+//calculate statistics based on data from csv file
 function calculateStatistics(array) {
     const returnObject = {};
 
     for (let key in array) {
         const curr = array[key];
         const matchesPlayed = parseFloat(curr[11]);
+        const points = curr[0] + curr[2] * 2 + curr[4] * 3;
         returnObject[key] = {
             "playerName": key,
             "gamesPlayed": matchesPlayed,
@@ -42,7 +46,7 @@ function calculateStatistics(array) {
                     "made": roundOneDecimal(curr[4] / matchesPlayed),
                     "shootingPercentage": roundOneDecimal(curr[4] / parseFloat(curr[5]) * 100.0),
                 },
-                "points": roundOneDecimal((curr[0] + curr[2] * 2 + curr[4] * 3) / matchesPlayed),
+                "points": roundOneDecimal(points / matchesPlayed),
                 "rebounds": roundOneDecimal(curr[6] / matchesPlayed),
                 "blocks": roundOneDecimal(curr[7] / matchesPlayed),
                 "assists": roundOneDecimal(curr[8] / matchesPlayed),
@@ -50,35 +54,42 @@ function calculateStatistics(array) {
                 "turnovers": roundOneDecimal(curr[10] / matchesPlayed)
             },
             "advanced": {
-                "valorization": roundOneDecimal(((curr[0] + curr[2] * 2 + curr[4] * 3 + curr[6] + curr[7] + curr[8] + curr[9])
+                "valorization": roundOneDecimal(((points + curr[6] + curr[7] + curr[8] + curr[9])
                     - (curr[1] - curr[0] + curr[3] - curr[2] + curr[5] - curr[4] + curr[10])) / matchesPlayed),
                 "effectiveFieldGoalPercentage": roundOneDecimal((curr[2] + 1.5 * curr[4]) / (curr[3] + curr[5]) * 100.0),
-                "trueShootingPercentage": roundOneDecimal((curr[0] + curr[2] * 2 + curr[4] * 3) / (2 * (curr[3] + curr[5] + 0.475 * curr[1])) * 100.0),
+                "trueShootingPercentage": roundOneDecimal(points / (2 * (curr[3] + curr[5] + 0.475 * curr[1])) * 100.0),
                 "hollingerAssistRatio": roundOneDecimal(curr[8] / (curr[3] + curr[5] + 0.475 * curr[1] + curr[8] + curr[10]) * 100.0)
             }
         };
     }
 
+    //final object - calculated stats ready for returning as a response
     return returnObject;
 }
 
 //start in-memory database
 const onServerStart = () => {
     return new Promise((resolve, reject) => {
+        //store data from csv file here
         let startData = {};
 
+        //read csv file and store data in an object with {key, value} pairs
+        //with key being playerFullName and value sum of every column, while
+        //pushing an element at the end for counting total matches played
         fs.createReadStream("./L9HomeworkChallengePlayersInput.csv")
             .pipe(parse({delimiter: ",", from_line: 2}))
             .on("data", function (row) {
                 const data = row.slice(2, 13).map(function (val) {
                     return parseInt(val);
                 });
+                //add match counter
                 data.push(1);
+                //update current player stats
                 startData = update(startData, data, row[0]);
             })
             .on("end", function () {
+                //calculate stats before returning object
                 const returnObject = calculateStatistics(startData);
-                console.log("In-memory database is UP!");
                 resolve(returnObject);
             })
             .on("error", function (err) {
